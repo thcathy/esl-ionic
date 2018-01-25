3ec6d83f4fd3084737ab375498c1f948b85de927
thcathy <thcathy@gmail.com>
2018-01-25 19:45:34 +0800

    draft dictation view page

--- src/pages/dictation-practice/dictation-practice.ts
+++ src/pages/dictation-practice/dictation-practice.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Dictation} from "../../entity/dictation";
 import {VocabPracticeService} from "../../providers/practice/vocab-practice.service";
@@ -17,9 +17,12 @@ import {VocabPractice} from "../../entity/voacb-practice";
   templateUrl: 'dictation-practice.html',
 })
 export class DictationPracticePage {
+  @ViewChild('speakRef') speakRef: ElementRef;
   dictation: Dictation;
   vocabPractices: VocabPractice[] = [];
   questionIndex: number;
+  phonics: string;
+  answer: string;
 
   constructor(
     public navCtrl: NavController,
@@ -28,6 +31,7 @@ export class DictationPracticePage {
   ) {
     this.dictation = navParams.get('dictation');
     this.questionIndex = 0;
+    this.phonics = "Phonetics";
 
     this.dictation.vocabs.forEach((vocab) => {
       vocabPracticeService.getQuestion(vocab.word, this.dictation.showImage)
@@ -37,8 +41,24 @@ export class DictationPracticePage {
     })
   }
 
+  speak() {
+    this.speakRef.nativeElement.src = this.vocabPractices[this.questionIndex].activePronounceLink;
+    this.speakRef.nativeElement.play();
+  }
+
+  showPhonics() {
+    if (this.currentQuestion().ipaunavailable) {
+      this.phonics = "N.A.";
+    } else {
+      this.phonics = this.currentQuestion().ipa;
+    }
+  }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad DictationPracticePage');
   }
 
+  currentQuestion() {
+    return this.vocabPractices[this.questionIndex];
+  }
 }
