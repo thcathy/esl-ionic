3d2a7eaa0cc6f92b0e52a4302c66454b8588ce1e
Timmy Wong <thcathy@gmail.com>
2018-03-25 20:32:27 +0800

    cleanup code

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
 
-/**
- * Generated class for the FooterComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
 @Component({
   selector: 'footer',
   templateUrl: 'footer.html'
@@ -15,8 +9,6 @@ export class FooterComponent {
   text: string;
 
   constructor() {
-    console.log('Hello FooterComponent Component');
-    this.text = 'Hello World';
   }
 
 }
