d5dd83c42357a7161ed194b1255ab9fdc053d81e
Timmy Wong <thcathy@gmail.com>
2018-05-28 15:59:55 +0800

    new API to get latest score cards

--- src/providers/ranking/ranking.service.ts
+++ src/providers/ranking/ranking.service.ts
@@ -7,6 +7,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import {MemberScoreRanking} from "../../entity/member-score-ranking";
 import { ENV } from '@environment';
+import {MemberScore} from "../../entity/member-score";
 
 
 
@@ -18,10 +19,14 @@ export class RankingService extends Service {
   }
 
   private randomTopScoreUrl = ENV.apiHost + '/ranking/random-top-score';
+  private allTimesAndLast6ScoreUrl = ENV.apiHost + '/member/ranking/score/alltimes-and-last6';
 
   randomTopScore(): Observable<MemberScoreRanking> {
     return this.http.get<MemberScoreRanking>(this.randomTopScoreUrl)
               .catch(this.handleError);
   }
 
+  allTimesAndLast6Score(): Observable<MemberScore[]> {
+    return this.http.get<MemberScore[]>(this.allTimesAndLast6ScoreUrl);
+  }
 }
