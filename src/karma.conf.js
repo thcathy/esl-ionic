0a46bc93010f3666998895cd8503528cb437e1ee
Timmy Wong <thcathy@gmail.com>
2018-12-18 21:16:54 +0800

    add kjhtml test report

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -9,6 +9,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-jasmine-html-reporter'),
       require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
@@ -20,7 +21,7 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['junit', 'progress'],
+    reporters: ['kjhtml', 'junit', 'progress'],
     junitReporter: {
       outputDir: '../junit',
       outputFile: 'test-results.xml',
