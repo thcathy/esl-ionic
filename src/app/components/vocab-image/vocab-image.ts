13f9325ee82bd99fd43a132b4e1838551c428607
Timmy Wong <thcathy@gmail.com>
2019-01-08 21:34:59 +0800

    - animation when changing vocabulary's image - update webview version

--- src/app/components/vocab-image/vocab-image.ts
+++ src/app/components/vocab-image/vocab-image.ts
@@ -1,13 +1,26 @@
 import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {animate, query, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'vocab-image',
   templateUrl: 'vocab-image.html',
   styleUrls: ['vocab-image.scss'],
+  animations: [
+    trigger('move', [
+      state('center', style({ transform: 'translateX(0%)' })),
+      state('left', style({ transform: 'translateX(-200%)' })),
+      state('right', style({ transform: 'translateX(200%)' })),
+      transition('* <=> *', [
+        animate(200)
+      ])
+    ])
+  ],
 })
 export class VocabImageComponent implements OnChanges {
-  @Input() images: String[];
+  @Input() images: string[];
   index: number;
+  state = 'center';
+  imageBase64: string = '';
 
   constructor() {
     this.index = 0;
@@ -15,16 +28,24 @@ export class VocabImageComponent implements OnChanges {
 
   ngOnChanges(_changes: SimpleChanges) {
     this.index = 0;
+    this.imageBase64 = this.images[0];
   }
 
   nextImage() {
+    this.state = 'right';
     this.index++;
     if (this.index >= this.images.length) this.index=0;
   }
 
   previousImage() {
+    this.state = 'left';
     this.index--;
     if (this.index < 0)
       this.index = this.images.length - 1;
+  }
+
+  onDone($event) {
+    this.imageBase64 = this.images[this.index];
+    this.state = 'center';
   }
 }
