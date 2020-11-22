8efcc51ed0396398726dbd91c6a4f80eee7fbf12
Timmy Wong <thcathy@gmail.com>
2020-11-22 18:01:49 +0800

    feature: support new server api

--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -4,6 +4,7 @@ import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {Observable} from 'rxjs/internal/Observable';
 import {NGXLogger} from 'ngx-logger';
+import {catchError} from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class ServerService {
@@ -14,11 +15,16 @@ export class ServerService {
   ) {
   }
 
-  private healthUrl = environment.apiHost + '/health';
+  private healthUrl1 = environment.apiHost + '/health';
+  private healthUrl2 = environment.apiHost + '/actuator/health';
 
   healthCheck(): Observable<String> {
     this.log.info(`Calling health check`);
-    return this.http.get<String>(this.healthUrl);
+    return this.http
+      .get<String>(this.healthUrl2)
+      .pipe(
+        catchError(() => this.http.get<String>(this.healthUrl1))
+      );
   }
 
 }
