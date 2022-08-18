64cd4622d1ca7284d07cc6caa39543adeeebcfc0
Timmy Wong <thcathy@gmail.com>
2022-08-18 21:24:29 +0800

    update cordova builder

--- src/environments/base-env.ts
+++ src/environments/base-env.ts
@@ -1,5 +1,5 @@
 export const baseEnv = {
-  version: 'v8.1.1',
+  version: 'v8.1.3',
   staticHost: 'https://friendlyeats-7fa2f.web.app',
   maxSentenceLength: 40,
   learntVocabularyMinimumCorrect: 5,
