7579c2314015f7dda7ff0afe2af9f49a666e9b0f
Timmy Wong <thcathy@gmail.com>
2022-09-10 10:32:13 +0800

    migrate to capacitor

--- build.sh
+++ build.sh
@@ -13,28 +13,36 @@ buildFirebase() {
 }
 
 buildios() {
- ionic cordova build ios --prod
- xed platforms/ios/FunFunSpell.xcworkspace/
+  setVersion
+  sed -i '' "s/MARKETING_VERSION = .*;/MARKETING_VERSION = $VERSION;/g" ios/App/App.xcodeproj/project.pbxproj
+  ionic capacitor build ios --prod  
 }
 
 buildAndroidApk() {
-  ionic cordova build android --prod --release
-  jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.jks  -storepass funfunspell platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk esl-dictation
+  ionic cap build android --prod --no-open
+  jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.jks  -storepass funfunspell android/app/build/outputs/apk/release/app-release-unsigned.apk esl-dictation
   rm -f esl-dictation.apk
   /Users/thcathy/Library/Android/sdk/build-tools/28.0.3/zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk esl-dictation.apk
   cp esl-dictation.apk ~/Google\ Drive/apk/
 }
 
 buildAndroidAab() {
-  ionic cordova build android --prod
-  cd platforms/android
+  setVersion  
+  ionic cap build android --prod --no-open
+  cd android
+  sed -i ''  "s/versionName \".*\"/versionName \"$VERSION\"/g" app/build.gradle
   ./gradlew bundle
-  cd ../..
-  jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.jks -storepass funfunspell platforms/android/app/build/outputs/bundle/release/app-release.aab esl-dictation
+  cd ..
+  jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.jks -storepass funfunspell android/app/build/outputs/bundle/release/app-release.aab esl-dictation
 }
 
 simios() {
  ionic cordova emulate ios -lc
+}
+
+setVersion() {
+  VERSION=`cat package.json | python3 -c "import sys, json; print(json.load(sys.stdin)['version'])"`
+  echo "set version=$VERSION"
 }
 
 "$@"
